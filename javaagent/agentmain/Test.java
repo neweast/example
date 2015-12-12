4313d7971b153fa71700e6e1dc46e41e9d3d0701
yuanquan.ld <yuanquan.ld@taobao.com>
2015-12-12 19:44:41 +0800

    add MANIFEST.MF and desc.md

--- javaagent/agentmain/Test.java
+++ javaagent/agentmain/Test.java
@@ -8,8 +8,10 @@ import com.sun.tools.attach.VirtualMachine;
 public class Test {
     public static void main(String[] args) throws AttachNotSupportedException,
             IOException, AgentLoadException, AgentInitializationException {
+		String agentJar = Test.class.getProtectionDomain().getCodeSource().getLocation().getFile() + "Test.jar";
+		agentJar = agentJar.substring(1,agentJar.length());
         VirtualMachine vm = VirtualMachine.attach(args[0]);
-        vm.loadAgent("D:\\code\\git\\test2\\Test.jar");
+        vm.loadAgent(agentJar);
 		vm.detach();
     }
 }
--- javaagent/agentmain/desc.md
+++ javaagent/agentmain/desc.md
@@ -10,11 +10,15 @@
 
 ## 第二步
 
+    jar cvfm Test.jar MANIFEST.MF *.class
+
+## 第三步
+
 启动TargetVM
 	
 	java TargetVM
 
-## 第三步
+## 第四步
 
 查看 TargetVM 进程号 PID，然后运行
 
